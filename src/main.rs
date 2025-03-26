fbbcd6c5bf6c3e89a142429610fc1ee2eed729cc
Bastien Sevajol <contact@bux.fr>
2025-03-26 14:37:22 +0100

    md

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{collections::HashSet, fs};
 
+const SIGNIFICANT: f32 = 250.0;
+
 fn main() {
     let raw = fs::read_to_string("data.csv").expect("Failed to read file");
     let headers: Vec<&str> = raw.lines().next().unwrap().split(";").collect();
@@ -11,12 +13,20 @@ fn main() {
     let categories = extract_unique_values(&raw, category_index);
     let sub_categories = extract_unique_values(&raw, sub_category_index);
 
-    println!("# Par catégories");
+    println!("# ➡️  Par catégories");
+    println!();
     print_totals(&raw, debit_index, category_index, &categories);
 
     println!();
-    println!("# Par sous catégories");
+    println!("# ➡️  Par sous catégories");
+    println!();
     print_totals(&raw, debit_index, sub_category_index, &sub_categories);
+}
+
+struct Item {
+    label: String,
+    sub_category: String,
+    value: f32,
 }
 
 fn find_index(headers: &[&str], target: &str) -> usize {
@@ -32,7 +42,14 @@ fn extract_unique_values(raw: &str, index: usize) -> Vec<String> {
         .collect()
 }
 
+// fn extract_column(raw: &str, column_index: usize) -> Vec<Item> {
+
+// }
+
 fn print_totals(raw: &str, debit_index: usize, index: usize, values: &[String]) {
+    let mut totals = vec![];
+    // let mut significant = vec![];
+
     for value in values {
         let total: f32 = raw.lines()
             .skip(1)
@@ -47,6 +64,14 @@ fn print_totals(raw: &str, debit_index: usize, index: usize, values: &[String]) 
             })
             .sum();
 
-        println!("{}: {}", value, total);
+        totals.push((value, total));
+        totals.sort_by(|a, b| ((a.1 * 1000.) as i32).cmp(&((b.1 * 1000.) as i32)));
     }
+
+    println!("||||");
+    println!("|-|-|-|");
+    for (name, total) in totals {
+        println!("|{}|{:>.2}|", name, total.abs());
+    }
+
 }
